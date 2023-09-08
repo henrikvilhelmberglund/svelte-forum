063e1e9da187192867645c476e215e9b0eab5db2
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-09-08 15:49:46 +0200

    feat: :sparkles: add GoBack component

---
+++ src/lib/components/GoBack.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<button
+	on:click={() => history.back()}
+	class="rounded bg-gray-500 px-2 py-2 font-semibold text-white hover:bg-gray-700">
+	Go Back
+</button>
+
+<style>
+</style>
